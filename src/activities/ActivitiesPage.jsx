282bd3313ce16f801c8972acf939bff6eababc8a
milandiaz <milandiaz2000@hotmail.com>
2025-07-14 12:19:51 -0500

    First commit

--- src/activities/ActivitiesPage.jsx
+++ src/activities/ActivitiesPage.jsx
@@ -1,8 +1,44 @@
+import useQuery from "../api/useQuery";
+import useMutation from "../api/useMutation";
+import { useAuth } from "../auth/AuthContext";
+
 export default function ActivitiesPage() {
+  const {
+    data: activities,
+    loading,
+    error,
+  } = useQuery("/activities", "activities");
+  // Mutation that will POST /players and invalidate the "puppies" tag
+  // I am renaming the data, loading, and error variables here because
+  // they will conflict with the query variables otherwise
+
+  const {
+    mutate,
+    data: addedActivity,
+    loading: adding,
+    error: addError,
+  } = useMutation("POST", "/activities", ["activities"]);
+  // /** Calls the mutate function with data from the form */
+  const addActivity = (formData) => {
+    const name = formData.get("name");
+    // breed is required to add a puppy
+    mutate({ name });
+  };
+
   return (
     <>
       <h1>Activities</h1>
-      <p>Imagine all the activities!</p>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      {Array.isArray(activities) &&
+        activities.map((activity) => <p key={activity.id}>{activity.name}</p>)}
+      <form action={addActivity}>
+        <label>
+          Name: <input name="name" />
+        </label>
+        <br />
+        <button>Submit</button>
+      </form>
     </>
   );
 }
---
+++ src/layout/Activities.jsx
@@ -0,0 +1,7 @@
+// import { useEffect, useState } from "react";
+// import { useAuth } from "./AuthContext";
+// import useQuery from "../api/useQuery";
+// import useMutation from "../api/useMutation";
+// import { useApi } from "../api/ApiContext";
+
+// export default function Activities() {}
